e4c95562d5bfc51b2b29fcd058b5a75673879445
Walter <walter.salas@me.com>
2020-06-14 12:40:13 -0500

    includes

---
+++ src/es7/index.js
@@ -0,0 +1,13 @@
+// en ES7 se incluye el "include" que es un metodo que recorre un arreglo y valida si esta dicho valor
+
+// arreglo de numerso
+let numbers = [2,3,5,6,7,4,2,3,4,5,6,7,7,5,4,3,3,4,5,6];
+
+// arreglo y metodo de includes (Valor) con eso el array sera escaneado en busqueda del valor definido
+if (numbers.includes(8)){
+    console.log('Si lo encontre');
+} else {
+    console.log('No esta este rollo!')
+};
+
+// ********* ************//
